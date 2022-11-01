6c49dd573cf52de5d6fa46a9ac80c0423d3d09ef
Sam Stephens <rsamuelstephens@gmail.com>
2022-11-01 14:29:40 -0400

    Commit Band Name Generator Project
    Day 1 project of Angela Yu's 100 Days of Python.

---
+++ band_name_generator.py
@@ -0,0 +1,12 @@
+# Day 1 of 100 Days of Python by Dr. Angela Yu
+
+# Creat a Band Name Generator using strings and variables
+
+# Printing a string that is the welcome message
+print("Welcome to the Band Name Generator!")
+# Taking input from the user that is stored in the city variable.
+city = input("What city were you born in?:\n> ")
+# Taking input from the user that is stored in the pet variable.
+pet = input("What's your pet's name?:\n> ")
+# Printing their suggested band name as an f-string adding the city and pet variables.
+print(f"Your band name could be {city} {pet}.")
